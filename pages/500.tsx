c518fc086c03f5e0a5a61ab92e69ae63a2e61536
BogdanGlazkov <bglazkov@i.ua>
2023-06-07 22:35:34 +0300

    Add error pages

---
+++ pages/500.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Oops! Error</h1>
+      <p>Something went wrong. Please refresh the page or contact support</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
